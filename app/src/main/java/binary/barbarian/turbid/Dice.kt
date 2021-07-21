58db4dd00fae80bed568b6b1dc1d86ab05973482
Harlen Hobbs <harlenhobbs3@gmail.com>
2021-07-21 17:16:58 -0400

    added custom background and cleaned up UI

--- app/src/main/java/binary/barbarian/turbid/Dice.kt
+++ app/src/main/java/binary/barbarian/turbid/Dice.kt
@@ -13,7 +13,7 @@ import binary.barbarian.turbid.databinding.FragmentFirstBinding
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class Dice : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
@@ -30,6 +30,20 @@ class FirstFragment : Fragment() {
         count++
         // Display the new value in the text view.
         showCountTextView.text = count.toString()
+    }
+
+    private fun rollDice(view: View, textId: Int, diceSize: Int) {
+        // Get the text view
+        val showDiceTextView = view.findViewById<TextView>(textId)
+        val random = java.util.Random()
+        var randomNumber = 0
+        if (diceSize > 0) {
+            randomNumber =  (1..diceSize).random() //random.nextInt(diceSize + 1)
+        }
+        //var roll = countString.toInt()
+
+        // Display the new value in the text view.
+        showDiceTextView.text = randomNumber.toString()
     }
 
     override fun onCreateView(
@@ -49,7 +63,7 @@ class FirstFragment : Fragment() {
         binding.randomButton.setOnClickListener {
             val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
             val currentCount = showCountTextView.text.toString().toInt()
-            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+            val action = DiceDirections.actionFirstFragmentToSecondFragment(currentCount)
             findNavController().navigate(action)
         }
 
@@ -60,6 +74,32 @@ class FirstFragment : Fragment() {
 
         binding.countButton.setOnClickListener{
             countMe(view)
+        }
+
+        binding.d100Button.setOnClickListener{
+
+            rollDice(view, R.id.textview_first_d100, 100)
+        }
+        binding.d20Button.setOnClickListener{
+            rollDice(view, R.id.textview_first_d20, 20)
+        }
+        binding.d12Button.setOnClickListener{
+            rollDice(view, R.id.textview_first_d12, 12)
+        }
+        binding.d10Button.setOnClickListener{
+            rollDice(view, R.id.textview_first_d10, 10)
+        }
+        binding.d8Button.setOnClickListener {
+            rollDice(view, R.id.textview_first_d8, 8)
+        }
+        binding.d6Button.setOnClickListener{
+            rollDice(view, R.id.textview_first_d6, 6)
+        }
+        binding.d4Button.setOnClickListener{
+            rollDice(view, R.id.textview_first_d4, 4)
+        }
+        binding.d2Button.setOnClickListener{
+            rollDice(view, R.id.textview_first_d2, 2)
         }
     }
 
